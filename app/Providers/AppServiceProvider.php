149cbe1a417e13cb6ece845230e32ca7e2bf6df0
mithileshtarkar <mithilesh.tarkar@ves.ac.in>
2018-12-05 00:23:15 +0530

    updated app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+
+
     }
 
     /**
@@ -26,5 +32,10 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
+
     }
 }
